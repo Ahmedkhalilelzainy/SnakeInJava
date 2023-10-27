979d9a43355d69080249f14a88c030aa5a3f7c92
Zedgamer9128 <zeeshantechnical9128@gmail.com>
2023-10-27 17:11:31 +0530

    Redraw Screen

--- src/main/java/com/zed/snake/App.java
+++ src/main/java/com/zed/snake/App.java
@@ -22,13 +22,48 @@ public class App {
     return term;
   }
 
-  void play() {
+  void hideCursor() {
+    System.out.print("\u001B[?25l");
+  }
+
+  void showCursor() {
+    System.out.print("\u001B[?25h");
+  }
+
+  void moveCursor(int x, int y) {
+    System.out.printf("\u001B[%d;%dH", x, y);
+  }
+
+  void clear() {
+    System.out.print("\u001B[2J");
+  }
+
+  void redraw(int height, int width) {
+    for (int i = 1; i <= height; i++) {
+      for (int j = 1; j <= width; j++) {
+        moveCursor(i, j);
+        System.out.print(" ");
+      }
+    }
+  }
+
+  void play() throws InterruptedException {
     TermSize term = getTermSize();
     int width = term.width;
     int height = term.height;
+    while (true) {
+      hideCursor();
+      redraw(height, width);
+      int x = width / 2;
+      int y = height / 2;
+      // moveCursor(x, y);
+      System.out.printf("\u001B[%d;%dH", x, y);
+      System.out.print("0");
+      Thread.sleep(1000);
+    }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws InterruptedException {
     App app = new App();
     app.play();
   };
---
+++ src/main/java/com/zed/snake/Terminlal.java
@@ -0,0 +1,4 @@
+package com.zed.snake;
+
+public class Terminlal {
+}
